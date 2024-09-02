dfd7ff537b509af54ee6abfe6c3d9aca01c3c82b
CarlosHerbertDev <hrbrtvieira@gmail.com>
2024-09-02 14:21:45 -0300

    alteração no Javascript, refatorando código

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,13 @@
-import { user } from "/src/scripts/services/user.js"
-import { repositories } from "/src/scripts/services/repositories.js"
+import { getUser } from "/src/scripts/services/user.js"
+import { getRepositories } from "/src/scripts/services/repositories.js"
+
+import {user} from '/src/scripts/objects/user.js' 
+import { screen } from '/src/scripts/objects/screen.js'
+
 
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
-    getUserProfile(userName)
+    getUserData(userName)
 })
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
@@ -11,41 +15,62 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     const key = e.which || e.keyCode
     const isEnterKeyPressed = key === 13
         if(isEnterKeyPressed) {
-        getUserProfile(userName)
+            getUserData(userName)
         }
 })
 
 
 
 
-function getUserProfile(userName){
-    user(userName).then(userData => {
-        let userInfo = `<div class="info">
-                        <img src="${userData.avatar_url}" alt="Foto do perfil do usuário" /> 
-                        <div class="data">
-                        <h1>${userData.name ?? 'Não possui nome cadastrado 😥'}</h1> 
-                        <p>${userData.bio ?? 'Não possui bio cadastrada 😥'}</p>
-                        </div>
-                        </div>`
+async function getUserData (userName) {
 
-        document.querySelector('.profile-data').innerHTML = userInfo
+    const userResponse = await getUser(userName)
+    const repositoriesResponse = await getRepositories(userName)
 
-        getUserRepositories(userName)
-    })
+    user.setInfo(userResponse)
+    user.setRepositories (repositoriesResponse)
+
+    screen.renderUser (user)
+
 }
 
-function getUserRepositories (userName) { 
-    repositories(userName).then(reposData => { 
+
+
+
+
+
+
+
+
+
+// function getUserProfile(userName){
+//     user(userName).then(userData => {
+//         let userInfo = `<div class="info">
+//                         <img src="${userData.avatar_url}" alt="Foto do perfil do usuário" /> 
+//                         <div class="data">
+//                         <h1>${userData.name ?? 'Não possui nome cadastrado 😥'}</h1> 
+//                         <p>${userData.bio ?? 'Não possui bio cadastrada 😥'}</p>
+//                         </div>
+//                         </div>`
+
+//         document.querySelector('.profile-data').innerHTML = userInfo
+
+//         getUserRepositories(userName)
+//     })
+// }
+
+// function getUserRepositories (userName) { 
+//     repositories(userName).then(reposData => { 
         
-        let repositoriesItens = ""
+//         let repositoriesItens = ""
 
-        reposData.forEach(repo => {
-            repositoriesItens += `<li><a href="${repo.html_url}">${repo.name}</a></li>`
-            })
+//         reposData.forEach(repo => {
+//             repositoriesItens += `<li><a href="${repo.html_url}">${repo.name}</a></li>`
+//             })
 
-            document.querySelector('.profile-data').innerHTML += `<div class="repositories section">
-                                                                    <h2>Repositórios</h2>
-                                                                    <ul>${repositoriesItens}</ul>
-                                                                </div>`
-    } )
-}   
+//             document.querySelector('.profile-data').innerHTML += `<div class="repositories section">
+//                                                                     <h2>Repositórios</h2>
+//                                                                     <ul>${repositoriesItens}</ul>
+//                                                                 </div>`
+//     } )
+// }   
